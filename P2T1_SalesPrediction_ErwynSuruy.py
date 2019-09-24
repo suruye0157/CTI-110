3e6f65d808c833445e0fd431ae7190eddcd0ea87
suruye0157 <55551319+suruye0157@users.noreply.github.com>
2019-09-24 12:40:25 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_ErwynSuruy.py
@@ -0,0 +1,24 @@
+# P2T1_SalesPrediction
+# 9/21/19
+# CTI-110 P2T1
+# Erwyn Suruy
+
+
+# Psuedocode
+
+# Enter amount of total sales
+# Display "enter amount of projected sales"
+# Input the total sales
+# Calculate the profit (total sales*0.23)
+# Get the projected total sales.
+# Display the profit
+
+total_sales=float(input('enter the projected sales: '))
+
+#Calculate the profit as 23 percentof the total sales.
+
+profit= total_sales*0.23
+
+#Display the profit.
+
+print("the profit is ",format(profit,'.2f'))
